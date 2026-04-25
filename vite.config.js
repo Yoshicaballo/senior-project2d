c40f709e896e32b2857e31ee0dd78a8e06640d5c
Yoshicaballo <jaydrien.cruz-rivera@innovationcharter.org>
2026-04-25 23:42:34 +0000

    JUST WORK

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 export default {
   base: "/senior-project2d/",
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
   },
   build: {
     target: "esnext",
     minify: false,
     sourcemap: true,
-  }
-}
+  },
+};
